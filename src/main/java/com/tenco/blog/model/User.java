427d271802a555382047cdde3ab0dd73545ebf91
DESKTOP-8N3GG4O\GGG <hand.jurajura@gmail.com>
2023-05-10 12:28:17 +0900

    JPA update 예외처리

--- src/main/java/com/tenco/blog/model/User.java
+++ src/main/java/com/tenco/blog/model/User.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
@@ -14,7 +15,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import lombok.Data;
 
 @Entity
-@Data  // !! (object Mapper 가) 파싱처리 할때 setter 반드시 있어야 한다.
+@Data // !! (object Mapper 가) 파싱처리 할때 setter 반드시 있어야 한다.
 public class User {
 
 	@Id
@@ -24,14 +25,16 @@ public class User {
 	@Column(nullable = false, length = 30)
 	private String username;
 
+	@NotBlank // null, "" - 빈문자열을 잡겠다
 	@Column(nullable = false, length = 100)
 	private String password;
 
+	@NotBlank
 	@Column(nullable = false, length = 50)
 	private String email;
-	
-	@ColumnDefault("'user'")  // 문자열 타입 명시는 큰따옴표 안에 작은따옴표해줘야함
-	private String role;  // user, admin, manager
+
+	@ColumnDefault("'user'") // 문자열 타입 명시는 큰따옴표 안에 작은따옴표해줘야함
+	private String role; // user, admin, manager
 
 	@CreationTimestamp // 자동 시간 입력 == now()
 	private Timestamp createdDate;
